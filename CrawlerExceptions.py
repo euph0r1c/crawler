989bc4a125d6090b7309a2232f25e72cb7c6655e
Stan Minaev <stan.minaev@gmail.com>
2016-09-27 19:14:01 +0200

    Delete CrawlerExceptions.py

--- CrawlerExceptions.py
+++
@@ -1,8 +0,0 @@
-# Exception that catches index page (par ex. index.html)
-class IndexPageException(EnvironmentError):
-    pass
-
-
-# Output type exception (if output type != 's', 'f' or 'd')
-class OutputTypeException(Exception):
-    pass
